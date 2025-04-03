06ff478d77f7f08284dbea2ff4222d3b3d504213
Sonupatel A L <144017814+Sonupatel15@users.noreply.github.com>
2025-04-03 11:27:54 +0530

    Update camera.py

--- ar_core/camera.py
+++ ar_core/camera.py
@@ -1,13 +1,3 @@
-# import cv2
-# import streamlit as st
-
-# def initialize_camera():
-#     cap = cv2.VideoCapture(0)
-#     if not cap.isOpened():
-#         st.error("Could not open webcam")
-#         return None
-#     return cap
-
 import cv2
 import streamlit as st
 import platform
